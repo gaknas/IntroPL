0a71707643990746b8b0cac9e2e4d14bde4431cc
Vladislav <dv2003pro@bk.ru>
2021-12-15 17:29:40 +0300

    Рассчет суммы внесен в отдельную функцию

--- Task 3/Task 3-2/main.cpp
+++ Task 3/Task 3-2/main.cpp
@@ -9,6 +9,13 @@
 size_t get_sequence_size(const std::string& message);
 
 /**
+ * \brief Рассчитывает сумму последовательность с количеством членов (\a size).
+ * \param size Количество членов последовательности.
+ * \return Сумма последовательности.
+ */
+double get_sequence_summ(const size_t size);
+
+/**
  * \brief Рассчитывает значение рекуррентного члена в зависимости от индекса (\a index).
  * \param index Индекс, номер итерации.
  * \return Значение рекуррентного члена.
@@ -25,17 +32,7 @@ int main() noexcept
     {
         setlocale(LC_ALL, "Russian");
         const auto size = get_sequence_size("Введите количество членов последовательности ");
-        double current = 0;
-        double summ = 0;
-        for (int i = 0; i < size; i++) {
-            if (i == 0) {
-                current = 1;
-            }
-            else {
-                current *= get_recurrent_member(i - 1);
-            }
-            summ += current;
-        }
+        const double summ = get_sequence_summ(size);
         std::cout << "Сумма последовательности из " << size << " членов равна: " << summ << std::endl;
         return 0;
     }
@@ -44,6 +41,21 @@ int main() noexcept
         std::cerr << e.what() << std::endl;
         return 1;
     }
+}
+
+double get_sequence_summ(const size_t size) {
+    double current = 0;
+    double summ = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (i == 0) {
+            current = 1;
+        }
+        else {
+            current *= get_recurrent_member(i - 1);
+        }
+        summ += current;
+    }
+    return summ;
 }
 
 size_t get_sequence_size(const std::string& message)
